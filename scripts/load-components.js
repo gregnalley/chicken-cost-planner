af965200aff2953d3749f442392b61236e281fb4
gregnalley <greg.nalley@gmail.com>
2026-06-28 16:14:33 -0400

    Add files via upload

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,1 +1,11 @@
+fetch('nav.html')
+.then(response => response.text())
+.then(data => {
+document.getElementById('navigation').innerHTML = data;
+});
 
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+document.getElementById('footer').innerHTML = data;
+});
