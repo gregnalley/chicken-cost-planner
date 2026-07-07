9b26c2e661d2aa7d36769a015938b9e5173e9368
gregnalley <greg.nalley@gmail.com>
2026-07-07 18:19:19 -0400

    Add files via upload

---
+++ assets/data/collections.js
@@ -0,0 +1,11 @@
+const BCP_COLLECTIONS = {
+  "COLLECTION-BROODER-STARTER": {
+    title: "🐣 Recommended Brooder Starter Setup",
+    description: "A simple starter collection for setting up a safe, warm, clean brooder for baby chicks.",
+    products: [
+      "PRD-029",
+      "PRD-030"
+      
+    ]
+  }
+};
