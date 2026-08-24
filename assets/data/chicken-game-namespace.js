946d14052f59474aff8c5e5499a570b71a309a2a
gregnalley <greg.nalley@gmail.com>
2026-08-24 11:16:27 -0400

    Add files via upload

---
+++ assets/data/chicken-game-namespace.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+(function(global){
+
+
+  const BCPChickenGame =
+    global.BCPChickenGame =
+      global.BCPChickenGame ||
+      {};
+
+
+  BCPChickenGame.version =
+    "0.1.0";
+
+
+})(window);
