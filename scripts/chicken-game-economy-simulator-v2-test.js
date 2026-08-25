d37418632ec7ccb5623fa0591d99cf72bb375203
gregnalley <greg.nalley@gmail.com>
2026-08-25 06:52:18 -0400

    Add files via upload

---
+++ scripts/chicken-game-economy-simulator-v2-test.js
@@ -0,0 +1,210 @@
+"use strict";
+
+
+(function(global){
+
+
+const BCPChickenGame =
+  global.BCPChickenGame;
+
+
+
+BCPChickenGame.economySimulatorV2 = {
+
+
+  createState:
+
+  function(){
+
+
+    const config =
+      BCPChickenGame.config;
+
+
+    return {
+
+
+      day:
+        1,
+
+
+      minute:
+        0,
+
+
+      money:
+        config.startingFarm.money,
+
+
+      eggs:
+        config.startingFarm.eggs,
+
+
+      chickens:
+        config.startingFarm.chickens,
+
+
+      coopCapacity:
+        config.coop.startingCapacity,
+
+
+      eggStorage:
+        config.storage.startingCapacity,
+
+
+      milestones:
+        []
+
+    };
+
+
+  },
+
+
+
+
+
+  tick:
+
+  function(
+    state
+  ){
+
+
+    const config =
+      BCPChickenGame.config;
+
+
+
+    /*
+      Advance time
+    */
+
+
+    state.minute += 1;
+
+
+
+    if(
+      state.minute >= 1440
+    ){
+
+      state.minute = 0;
+
+      state.day += 1;
+
+    }
+
+
+
+
+
+    /*
+      Produce eggs
+
+      1 egg per chicken
+      per game minute
+    */
+
+
+    const eggsProduced =
+      state.chickens *
+      config.chicken.eggsPerMinute;
+
+
+
+    state.eggs +=
+      eggsProduced;
+
+
+
+    /*
+      Prevent storage overflow
+    */
+
+
+    if(
+      state.eggs >
+      state.eggStorage
+    ){
+
+      state.eggs =
+        state.eggStorage;
+
+    }
+
+
+
+
+
+    /*
+      Sell eggs automatically
+    */
+
+
+    const income =
+      state.eggs *
+      config.eggs.sellValue;
+
+
+
+    state.money +=
+      income;
+
+
+
+    state.eggs =
+      0;
+
+
+
+  },
+
+
+
+
+
+  simulate:
+
+  function(
+    minutes
+  ){
+
+
+    const state =
+      this.createState();
+
+
+
+    for(
+      let i = 0;
+      i < minutes;
+      i++
+    ){
+
+      this.tick(
+        state
+      );
+
+    }
+
+
+
+    return state;
+
+
+  }
+
+
+
+};
+
+
+
+console.log(
+  "Chicken Economy Simulator V2 Loaded"
+);
+
+
+
+})(window);
