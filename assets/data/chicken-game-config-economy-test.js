ae6b9aa4972c3780ae296cfe0ad27802b4ef60f4
gregnalley <greg.nalley@gmail.com>
2026-08-25 16:25:44 -0400

    Add files via upload

---
+++ assets/data/chicken-game-config-economy-test.js
@@ -0,0 +1,358 @@
+"use strict";
+
+
+(function(global){
+
+
+const BCPChickenGame =
+  global.BCPChickenGame ||
+  {};
+
+
+
+BCPChickenGame.economyConfig = {
+
+
+  version:
+    "0.3.0-economy-test",
+
+
+
+  /*
+    Starting farm
+  */
+
+  startingFarm:
+
+  {
+
+    cash:
+      25,
+
+
+    hens:
+      3,
+
+
+    eggStorageCapacity:
+      250
+
+  },
+
+
+
+  /*
+    Chicken production
+
+    Game balance values,
+    not real-world values.
+
+  */
+
+  chicken:
+
+  {
+
+    purchaseCost:
+      50,
+
+
+    startingHenCapacity:
+      10
+
+  },
+
+
+
+  eggs:
+
+  {
+
+    secondsPerEggPerHen:
+      60,
+
+
+    startingValue:
+      1
+
+  },
+
+
+
+  /*
+    Storage system
+  */
+
+  storage:
+
+  {
+
+    upgrades:
+
+    [
+
+      {
+
+        level:
+          2,
+
+        cost:
+          300,
+
+        capacity:
+          750
+
+      },
+
+
+      {
+
+        level:
+          3,
+
+        cost:
+          1000,
+
+        capacity:
+          2000
+
+      },
+
+
+      {
+
+        level:
+          4,
+
+        cost:
+          3000,
+
+        capacity:
+          5000
+
+      }
+
+    ]
+
+  },
+
+
+
+  /*
+    Coop upgrades
+  */
+
+  coop:
+
+  {
+
+    upgrades:
+
+    [
+
+      {
+
+        level:
+          2,
+
+        cost:
+          250,
+
+        capacity:
+          25
+
+      },
+
+
+      {
+
+        level:
+          3,
+
+        cost:
+          1000,
+
+        capacity:
+          50
+
+      },
+
+
+      {
+
+        level:
+          4,
+
+        cost:
+          5000,
+
+        capacity:
+          100
+
+      }
+
+    ]
+
+  },
+
+
+
+  /*
+    Truck system
+
+  */
+
+  transportation:
+
+  {
+
+    starterTruckCapacity:
+      100,
+
+
+    starterTruckCycleSeconds:
+      45,
+
+
+    upgrades:
+
+    [
+
+      {
+
+        level:
+          2,
+
+        cost:
+          500,
+
+        capacity:
+          250
+
+      },
+
+
+      {
+
+        level:
+          3,
+
+        cost:
+          1500,
+
+        capacity:
+          500
+
+      }
+
+
+    ]
+
+  },
+
+
+
+  /*
+    Land expansion
+
+  */
+
+  land:
+
+  {
+
+    firstExpansion:
+
+    {
+
+      name:
+        "East Pasture",
+
+
+      cost:
+        3000,
+
+
+      targetUnlockMinutes:
+        30
+
+    }
+
+  },
+
+
+
+  /*
+    Time conversion
+
+  */
+
+  time:
+
+  {
+
+    realMinutesPerGameDay:
+      10
+
+  },
+
+
+
+  /*
+    Predator system
+
+  */
+
+  predators:
+
+  {
+
+    startingProtection:
+      0,
+
+
+    henLossMinute:
+      20,
+
+
+    henLossAmount:
+      1
+
+  },
+
+
+
+  /*
+    Feed placeholder
+
+    Added now so the
+    simulator has room
+    to grow.
+
+  */
+
+  feed:
+
+  {
+
+    enabled:
+      false,
+
+
+    costPerHenPerDay:
+      0
+
+  }
+
+
+
+};
+
+
+
+global.BCPChickenGameEconomyConfig =
+  BCPChickenGame.economyConfig;
+
+
+
+})(window);
