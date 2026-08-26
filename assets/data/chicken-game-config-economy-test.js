6ecd8e6c07b907a8a29d1010c0e864a472fb541e
gregnalley <greg.nalley@gmail.com>
2026-08-26 15:00:18 -0400

    Add files via upload

--- assets/data/chicken-game-config-economy-test.js
+++ assets/data/chicken-game-config-economy-test.js
@@ -217,41 +217,13 @@ feed:
 
 {
 
-  /*
-    Starting feed inventory.
-
-    The player begins with enough
-    feed that the first few hens
-    are not immediately stressful.
-  */
-
   startingAmount:
     100,
 
 
-  /*
-    Feed consumed by one hen
-    per game day.
-
-    First-pass test value only.
-    We will tune this after the
-    simulator begins tracking feed.
-  */
-
   poundsPerHenPerGameDay:
     0.25,
 
-
-  /*
-    Basic feed purchase.
-
-    Later we can add:
-    - larger bags
-    - bulk feed
-    - discounted purchases
-    - storage limits
-    - feed efficiency upgrades
-  */
 
   purchaseOptions:
 
@@ -292,15 +264,6 @@ feed:
 
   ],
 
-
-  /*
-    Early predator nuisance event.
-
-    This does NOT kill hens.
-
-    Predator losses are initially
-    limited to feed/resource loss.
-  */
 
   predatorFeedLoss:
 
